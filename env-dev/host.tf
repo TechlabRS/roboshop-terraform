6bc29af83f9c87f322ac4004ec2ddeef0e709a30
TechlabRS <129305203+TechlabRS@users.noreply.github.com>
2023-08-12 18:00:36 +0530

    Delete host.tf

--- env-dev/host.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_route53_zone" "myzone" {
-  name = "uknowme.tech"
-}
-
-resource "aws_kms_key" "mykey" {
-  description             = "my key for kms"
-  deletion_window_in_days = 7
-}
